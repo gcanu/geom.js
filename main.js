ca8d19bbf9efd555a002806fc628b6e19dce26f2
Guillaume Canu <guy.canu@gmail.com>
2013-11-29 11:29:09 +0100

    Modify project config

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var Circle = require('./circle.js');
+var Square = require('./square.js');
 
 var c = new Circle(1);
 console.log("Area : "+c.area());
 console.log("Circ : "+c.circumference());
+
+var s = new Square(10);
+console.log("Area : "+s.area());
+console.log("Peri : "+s.perimeter());
+console.log("Diag : "+s.diagonal());
